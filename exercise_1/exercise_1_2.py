e8ce61e9783c1285110d931ea0e2c0d7cf7dd4c9
Fabian <fafreibe@htwg-kosntanz.de>
2017-11-07 11:27:25 +0100

    gradient descent test script

---
+++ exercise_1/exercise_1_2.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from scipy import misc
+
+def read_imgs():
+
+    pass
+
+
+def exercise_1_2a():
+    pass
+
+
+if __name__ == '__main__':
+    exercise_1_2a()
---
+++ exercise_1/exercise_1_3a.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+# theta1 = tf.Variable([2], dtype=tf.float32)
+# theta2 = tf.Variable([1], dtype=tf.float32)
+# theta3 = tf.Variable([0], dtype=tf.float32)
+
+theta = tf.Variable( [ 2, 1, 0 ], dtype=tf.float32 )
+
+loss = ( 2 * theta[0] ** 2) + (4 * theta[1]) + tf.maximum(.0, theta[1] + theta[2] )
+
+init = tf.global_variables_initializer()
+
+print( theta[0] )
+
+sess = tf.Session()
+
+sess.run( init )
+
+optimizer = tf.train.GradientDescentOptimizer( .5 )
+
+train = optimizer.minimize( loss )
+
+for i in range(2):
+    sess.run( train )
+    print( sess.run( theta ) )
