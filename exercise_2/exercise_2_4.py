01bc891e314e4979134a80f854698f9263dc0b3d
Dennis Grießer <degriess@htwg-konstanz.de>
2017-12-03 22:12:23 +0100

    ready for submission

--- exercise_2/exercise_2_4.py
+++ exercise_2/exercise_2_4.py
@@ -10,14 +10,20 @@ def reshape(data):
     return np.reshape(data, [-1, int(np.sqrt(data.shape)), int(np.sqrt(data.shape)), 1])
 
 
-def weight(shape):
-    initial_weight = tf.truncated_normal(shape, stddev=.1)
-    return tf.Variable(initial_weight, trainable=False)
+def weight(shape, name='', trainable=True):
+    if trainable:
+        initial_weight = tf.truncated_normal(shape, stddev=.1)
+    else:
+        initial_weight = np.load(_params_file + '.npz')[name]
+    return tf.Variable(initial_weight, trainable=trainable)
 
 
-def bias(shape):
-    initial_bias = tf.constant(.1, shape=shape)
-    return tf.Variable(initial_bias, trainable=False)
+def bias(shape, name='', trainable=True):
+    if trainable:
+        initial_bias = tf.constant(.1, shape=shape)
+    else:
+        initial_bias = np.load(_params_file + '.npz')[name]
+    return tf.Variable(initial_bias, trainable=trainable)
 
 
 def conv2d(x, w):
@@ -44,63 +50,61 @@ def minibatches(inputs, targets, mbs, shuffle):
 def main(file):
 
     eta = 1e-4
-    lambda_ = 0.04
-    mbs = 10
-    epochs = 25
-    graphs = True
-    num_classes = 10
-    two_convs = True
-    test_acc_list_one_conv = []
-    test_acc_list_two_convs = []
+    lambda_ = 600
+    filename = 'output_layer'
 
+    # initialize variable input
+    x = tf.Variable(tf.constant(.1, shape=[1, 28, 28, 1]), dtype=tf.float32)
 
-    logger.info('loading data from file  {}'.format(file))
-    x_tr, y_tr, x_te, y_te = load_data(file, num_classes)
-    x_tr = x_tr[0]
-    y_tr = y_tr[0]
-    logger.info('loading finished')
-
-    x = tf.Variable(tf.constant(x_tr, shape=(1, 28, 28, 1)))
-    #x = tf.placeholder(tf.float32, [None, 28, 28, 1])
-    y = tf.placeholder(tf.float32, [None, 10])
-
-    w_conv1 = weight([5, 5, 1, 32])
-    b_conv1 = bias([32])
+    w_conv1 = weight([5, 5, 1, 32], 'w_conv1', False)
+    b_conv1 = bias([32], 'b_conv1', False)
     h_conv1 = tf.nn.relu(conv2d(x, w_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-
-    w_conv2 = weight([5, 5, 32, 64])
-    b_conv2 = bias([64])
+    w_conv2 = weight([5, 5, 32, 64], 'w_conv2', False)
+    b_conv2 = bias([64], 'b_conv2', False)
     h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
-    w_fc = weight([7 * 7 * 64, 1024])
-    b_fc = bias([1024])
 
+    w_fc = weight([7 * 7 * 64, 1024], 'w_fc', False)
+    b_fc = bias([1024], 'b_fc', False)
     h_pool_fc = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc = tf.nn.relu(tf.matmul(h_pool_fc, w_fc) + b_fc)
 
-    w_out = weight([1024, 10])
-    b_out = bias([10])
+    w_out = weight([1024, 10], 'w_out', False)
+    b_out = bias([10], 'b_out', False)
 
     pred = tf.matmul(h_fc, w_out) + b_out
 
-    loss = lambda_ * tf.reduce_mean(x)**2 - pred[0][0]
+    loss = lambda_ * tf.reduce_mean(x**2) - pred[0][0]
+    # optimize for activation of first unit in output layer
     train = tf.train.GradientDescentOptimizer(eta).minimize(loss)
 
     with tf.Session() as sess:
-        x = tf.Variable(tf.constant(x_tr, shape=(1, 28, 28, 1)))
         sess.run(tf.global_variables_initializer())
+
+        img = sess.run(x).reshape((28,28))
+        plt.subplot(2, 2, 1)
+        plt.title('before')
+        plt.imshow(img, cmap='Greys_r')
+        plt.axis('off')
+
         for i in range(100):
             print(sess.run(loss))
-            sess.run(train, {x: sess.run(x)})
-        plt.imshow(sess.run(x).reshape((28, 28)), cmap='Greys_r')
-        plt.show()
+            sess.run(train)
 
+        img2 = sess.run(x).reshape((28, 28))
+        plt.subplot(2, 2, 2)
+        plt.title('after')
+        plt.imshow(img2, cmap='Greys_r')
+        plt.axis('off')
+        plt.savefig('maximum_activation_neuron_in_{}.png'.format(filename))
 
 if __name__ == '__main__':
 
-    logger = logging.getLogger('ex2_2')
+    _params_file='all_params'
+
+    logger = logging.getLogger('ex2_4')
     logger.setLevel(logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler())
 
