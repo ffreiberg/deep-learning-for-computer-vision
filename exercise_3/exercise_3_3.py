1e5a4c2eed2553a6ca1b7c317be04d037c3d24f2
Fabi <king-fabi@gmx.de>
2018-01-05 13:26:38 +0100

    you know the deal

--- exercise_3/exercise_3_3.py
+++ exercise_3/exercise_3_3.py
@@ -195,15 +195,15 @@ def ex_batch_norm(file):
 
     w_conv1 = weight([5, 5, 1, 32])
     o_conv1= conv2d(x, w_conv1)
-    bn_conv1 = tf.contrib.layers.batch_norm(o_conv1, center=True, scale=True, is_training=is_training)
     # bn_conv1 = tf.contrib.layers.batch_norm(o_conv1, center=True, scale=True, is_training=is_training)
+    bn_conv1 = tf.layers.batch_normalization(o_conv1, center=True, scale=True, training=is_training)
     h_conv1 = leaky_relu(bn_conv1, a)
     h_pool1 = max_pool_2x2(h_conv1)
 
     w_conv2 = weight([5, 5, 32, 64])
     o_conv2 = conv2d(h_pool1, w_conv2)
-    bn_conv2 = tf.contrib.layers.batch_norm(o_conv2, center=True, scale=True, is_training=is_training)
     # bn_conv2 = tf.contrib.layers.batch_norm(o_conv2, center=True, scale=True, is_training=is_training)
+    bn_conv2 = tf.layers.batch_normalization(o_conv2, center=True, scale=True, training=is_training)
     h_conv2 = leaky_relu(bn_conv2, a)
     h_pool2 = max_pool_2x2(h_conv2)
 
@@ -211,7 +211,7 @@ def ex_batch_norm(file):
     h_pool_fc = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     o_fc = tf.matmul(h_pool_fc, w_fc)
     # bn_fc = tf.contrib.layers.batch_norm(o_fc, center=True, scale=True, is_training=is_training)
-    # bn_fc = tf.contrib.layers.batch_norm(o_fc, center=True, scale=True, is_training=is_training)
+    bn_fc = tf.layers.batch_normalization(o_fc, center=True, scale=True, training=is_training)
     h_fc = leaky_relu(o_fc, a)
 
     h_dropout = tf.layers.dropout(h_fc, rate=dropout, training=is_training)
