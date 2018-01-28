2b351b88d70a490d0dfc75cd81e004825193bd37
Dennis Grießer <degriess@htwg-konstanz.de>
2018-01-28 20:11:11 +0100

    finished a-d

--- exercise_4/exercise_4.2.py
+++ exercise_4/exercise_4.2.py
@@ -110,6 +110,8 @@ def main():
     y_cyr = one_hot(y_cyr, 14)
     y_lat = one_hot(y_lat, 14)
 
+    is_training = tf.placeholder(tf.bool, name='phase')
+
     test_inputs_x, test_inputs_y, test_targets_x, test_targets_y, train_inputs_x, train_inputs_y, train_targets_x, train_targets_y = separate_test_and_training(x_cyr, y_cyr, x_lat, y_lat, 500)
 
 
@@ -124,18 +126,21 @@ def main():
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     conv1 = conv2d(x, W_conv1, [1, 2, 2, 1]) + b_conv1
-    h_conv1 = tf.nn.relu(conv1)
+    batch1 = tf.contrib.layers.batch_norm(conv1, center=True, scale=True, is_training=is_training)
+    h_conv1 = tf.nn.relu(batch1)
 
 
     W_conv2 = weight_variable([3, 3, 32, 64])
     b_conv2 = bias_variable([64])
     conv2 = conv2d(h_conv1, W_conv2, [1, 4, 4, 1]) + b_conv2
-    h_conv2 = tf.nn.relu(conv2)
+    batch2 = tf.contrib.layers.batch_norm(conv2, center=True, scale=True, is_training=is_training)
+    h_conv2 = tf.nn.relu(batch2)
 
 
     W_conv3 = weight_variable([3, 3, 64, 128])
     b_conv3 = bias_variable([128])
     conv3 = conv2d(h_conv2, W_conv3, [1, 4, 4, 1]) + b_conv3
+    batch3 = tf.contrib.layers.batch_norm(conv3, center=True, scale=True, is_training=is_training)
     h_conv3 = tf.nn.relu(conv3)
 
 
@@ -144,27 +149,32 @@ def main():
     W_trans_conv3 = weight_variable([3, 3, 64, 128])
     b_trans_conv3 = bias_variable([64])
     trans_conv3 = conv2d_transpose(h_conv3, W_trans_conv3, [1, 4, 4, 1], [minibatchsize, 4, 4, 64]) + b_trans_conv3
-    h_trans_conv3 = tf.nn.relu(trans_conv3)
+    batch_trans3 = tf.contrib.layers.batch_norm(trans_conv3, center=True, scale=True, is_training=is_training)
+    h_trans_conv3 = tf.nn.relu(batch_trans3)
 
 
     W_trans_conv2 = weight_variable([3, 3, 32, 64])
     b_trans_conv2 = bias_variable([32])
     trans_conv2 = conv2d_transpose(h_trans_conv3, W_trans_conv2, [1, 4, 4, 1], [minibatchsize, 14, 14, 32]) + b_trans_conv2
-    h_trans_conv2 = tf.nn.relu(trans_conv2)
+    batch_trans2 = tf.contrib.layers.batch_norm(trans_conv2, center=True, scale=True, is_training=is_training)
+    h_trans_conv2 = tf.nn.relu(batch_trans2)
 
 
     W_trans_conv1 = weight_variable([5, 5, 1, 32])
     b_trans_conv1 = bias_variable([1])
     trans_conv1 = conv2d_transpose(h_trans_conv2, W_trans_conv1, [1, 2, 2, 1], [minibatchsize, 28, 28, 1]) + b_trans_conv1
-    h_trans_conv1 = tf.nn.relu(trans_conv1)
+    batch_trans1 = tf.contrib.layers.batch_norm(trans_conv1, center=True, scale=True, is_training=is_training)
+    h_trans_conv1 = tf.nn.relu(batch_trans1)
 
 
 
-    #------------Wird aktuell nicht verwendet------------------
+    #------------Fully Connected with dropout and reshape layer------------------
     h_flat = tf.reshape(h_trans_conv1, [-1, 28 * 28])
     W_fc1 = weight_variable([28 * 28, 1024])
     b_fc1 = bias_variable([1024])
-    h_fc1 = tf.nn.relu(tf.matmul(h_flat, W_fc1) + b_fc1)
+    fc1 = tf.matmul(h_flat, W_fc1) + b_fc1
+    batch_fc1 = tf.contrib.layers.batch_norm(fc1, center=True, scale=True, is_training=is_training)
+    h_fc1 = tf.nn.relu(batch_fc1)
 
 
     keep_prob = tf.placeholder(tf.float32)
@@ -174,26 +184,29 @@ def main():
     W_fc2 = weight_variable([1024, 784])
     b_fc2 = bias_variable([784])
     h_fc2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-    #----------------------------------------------------------
 
+
+    #-----------Minimize MSE Loss-function with Adam-----------
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     loss = tf.losses.mean_squared_error(labels=tf.reshape(y_, [-1, 28 * 28]), predictions=h_fc2)
-    train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
+    with tf.control_dependencies(update_ops):
+        train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for e in range(1):
+        for e in range(250):
             print("Epoch: ", e)
             for i, batch in enumerate(minibatches(train_inputs_x, train_inputs_y, train_targets_x, train_targets_y, minibatchsize, True)):
                 batch_cyr, batch_lat = batch
                 batch_cyr = batch_cyr.reshape((minibatchsize, 28, 28, 1))
                 batch_lat = batch_lat.reshape((minibatchsize, 28, 28, 1))
-                train_step.run(feed_dict={x: batch_cyr, y_: batch_lat, keep_prob: 0.5})
-            print(sess.run(loss, feed_dict={x: batch_cyr, y_: batch_lat, keep_prob: 0.5}))
+                train_step.run(feed_dict={x: batch_cyr, y_: batch_lat, keep_prob: 0.5, is_training: True})
+            print("Loss: ", sess.run(loss, feed_dict={x: batch_cyr, y_: batch_lat, keep_prob: 0.5, is_training: True}))
 
-        output = sess.run(h_fc2, feed_dict={x: test_inputs_x[0:300].reshape((300, 28,28,1)), keep_prob: 1.0})
+        output = sess.run(h_fc2, feed_dict={x: test_inputs_x[0:300].reshape((300, 28,28,1)), keep_prob: 1.0, is_training: False})
 
 
-
+    #-----------show test results as plot -------------
     plt.figure(1)
     counter = 331
     for i in range(1, 4):
